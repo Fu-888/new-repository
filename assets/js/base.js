c5927cd0b99dcc7ac1e0d021f19c9bd0a6fb9499
jyb <3257223409@@qq.com>
2022-05-30 20:56:38 +0800

    完善了个人中心

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,7 +1,29 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter((option)=>{
+$.ajaxPrefilter((option) => {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   option.url = `http://www.liulongbin.top:3007` + option.url;
-})
+
+  // 判断URL是否有/my/
+  if (option.url.includes('/my/')) {
+    option.headers = {
+      Authorization: localStorage.getItem('token'),
+    };
+  }
+
+  // 控制用户的访问权限
+  // 请求之后进行校验 token
+  option.complete = (res) => {
+    console.log(res);
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      // 清空
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+    }
+  };
+});
---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+// 获取用户的基本信息
+function getUserInfo() {
+  $.ajax({
+    type: 'GET',
+    url: '/my/userinfo',
+    // headers: {
+    //   Authorization: localStorage.getItem('token'),
+    // },
+    success(res) {
+    //   console.log(res);
+      if (res.status !== 0) return layer.msg('获取用户信息失败');
+      layer.msg('获取用户信息成功');
+      renderAvatar(res.data);
+    },
+  });
+}
+
+getUserInfo();
+
+// 渲染数据
+const renderAvatar = (user) => {
+  // console.log(user);
+
+  //获取名字
+  let uname = user.nickname || user.username;
+  // 渲染欢迎语
+  $('#welcome').html(`欢迎 ${uname}`);
+
+  if (user.user_pic !== null) {
+    // 渲染图片
+    $('.layui-nav-img').attr('src', user.user_pic);
+    $('.text-avatar').hide();
+  } else {
+    // 渲染文本
+    $('.layui-nav-img').hide();
+    $('.text-avatar').html(uname[0].toUpperCase());
+  }
+};
+
+// 退出
+$('#btnout').on('click', function(){
+    // console.log(11);
+    layui.layer.confirm(
+      '确定退出登录？',
+      { icon: 3, title: '' },
+      function () {
+        // 清空本地存储里面的 token
+        localStorage.removeItem('token');
+        // 重新跳转到登录页面
+        location.href = '/login.html';
+      }
+    );
+});
